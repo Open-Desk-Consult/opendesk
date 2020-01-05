9ab812a36160f513312dc520c5c20d3a903cbfce
devMarshall <onyendo52@gmail.com>
2020-01-05 20:40:18 +0100

    chore: build services

---
+++ src/screens/Services/Wrapper.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+
+  .content {
+    width: 90%;
+    font-size: 35px;
+    display: flex;
+    flex-direction: row;
+    align-self: flex-end;
+    .col {
+      width: 50%;
+
+      .heading {
+        margin-bottom: 77px;
+      }
+      .description {
+        width: 80%;
+        margin-bottom: 41px;
+      }
+      .features {
+        color: #999999;
+        font-size: 18px;
+        .item {
+          margin-bottom: 36px;
+        }
+      }
+    }
+  }
+`;
--- src/screens/Services/index.js
+++ src/screens/Services/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import Wrapper from './Wrapper';
 
 export default () => {
-  return <h2>Services</h2>;
+  return (
+    <Wrapper>
+      <div className="content">
+        <div className="col">
+          <div className="heading">Design</div>
+          <div className="description">
+            Balancing functionality and creativity to deliver brand and digital experiences that
+            enhance your projects vision
+          </div>
+          <div className="features">
+            <div className="item">UI/UX Design</div>
+            <div className="item">Visual Design</div>
+            <div className="item">Project Management</div>
+            <div className="item">Business solutions consultancy</div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="heading">Design</div>
+          <div className="description">
+            Pushing digital development boundaries while creating functional experiences that work
+            for our partners and their customers
+          </div>
+          <div className="features">
+            <div className="item">Web Development</div>
+            <div className="item">Technical Support</div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
