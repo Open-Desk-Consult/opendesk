de544fb6d989f3175f4b73991f2275f4b36f9cfd
devMarshall <onyendo52@gmail.com>
2020-01-13 16:45:57 +0100

    chore: scaffold product top section

--- src/screens/Project/Wrapper.js
+++ src/screens/Project/Wrapper.js
@@ -1,6 +1,39 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  background-color: red;
   height: 100%;
+  font-size: 16px;
+  padding-left: 152px;
+
+  .section {
+    margin-bottom: 10vh;
+  }
+
+  .top_section {
+    display: flex;
+    height: 100vh;
+
+    flex-display: row;
+
+    justify-content: space-between;
+    .content {
+      width: 37.5%;
+      padding-top: 20vh;
+      .header {
+        font-weight: bold;
+        font-size: 28px;
+        margin-bottom: 85px;
+      }
+      .text {
+        line-height: 40px;
+      }
+    }
+
+    .product_image {
+      width: 45%;
+      height: 100vh;
+      object-fit: cover;
+      object-position: center;
+    }
+  }
 `;
--- src/screens/Project/index.js
+++ src/screens/Project/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import Wrapper from './Wrapper';
+import hero from '../../assets/images/product-hero.jpg';
 
 export default () => {
-  return <Wrapper>Project Page</Wrapper>;
+  return (
+    <Wrapper>
+      <div className="section top_section">
+        <div className="content">
+          <div className="header">Fleet Management System</div>
+          <div className="text">
+            lorem ipsum dolor sit amet et dolor magnus libra mango lorem ipsum dolor sit amet et
+            dolor magnus libra mango lorem ipsum dolor sit amet et dolor magnus libra mango lorem
+            ipsum dolor sit amet et dolor magnus libra mango lorem ipsum dolor sit amet et dolor
+            magnus libra mango lorem ipsum dolor sit amet et dolor magnus libra mango lorem ipsum
+            dolor sit amet et dolor magnus libra mango. lorem ipsum dolor sit amet et dolor magnus
+            libra mango lorem ipsum dolor sit amet et dolor magnus libra mango lorem ipsum dolor sit
+            amet et dolor magnus libra mango. lorem ipsum dolor sit amet et dolor magnus libra
+            mango lorem ipsum dolor sit amet et dolor magnus libra mango lorem ipsum dolor sit amet
+            et dolor magnus libra mango. lorem ipsum dolor sit amet et dolor magnus libra mango
+            lorem ipsum dolor sit amet et dolor magnus libra mango lorem ipsum dolor sit amet et
+            dolor magnus libra mango
+          </div>
+        </div>
+        <img className="product_image" alt="product-hero" src={hero} />
+      </div>
+      <div className="section">Section 2</div>
+      <div className="section">Section 3</div>
+    </Wrapper>
+  );
 };
